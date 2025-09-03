6bb01ec0bf7034f1081df9912f685ecb703c5822
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-09-03 18:11:27 -0400

    feat: cria a estrutura de pastas e a página de login

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { FileText, Lock, Eye } from "lucide-react";
+
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+            <div className="w-full max-w-sm p-8 space-y-6 bg-white rounded-xl shadow-lg">
+                <img className="block mx-auto h-25 w-25 mh" src="/logo.png" />
+                <div className="text-center">
+
+                    <h1 className="text-2xl font-bold text-black">
+                        Mural - Guarda Mirim
+                    </h1>
+                </div>
+
+                <form className="space-y-4">
+                    { }
+                    <div className="relative">
+                        <FileText className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500  " />
+                        <Input
+                            id="cpf" type="text" required
+                            placeholder="000.000.000-00"
+                            className="pl-10 "
+                        />
+
+                    </div>
+
+                    { }
+                    <div className="relative">
+                        <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500" />
+                        <Input
+                            id="password" type="password" required
+                            placeholder="Password"
+                            className="pl-10 pr-10"
+                        />
+                        <Eye className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5  cursor-pointer text-gray-500" />
+                    </div>
+
+                    <Button type="submit" className="w-full !mt-6 bg-blue-600 text-white hover:bg-blue-700">
+                        Entrar
+                    </Button>
+                </form>
+
+                <div className="text-center">
+                    <a href="#" className="text-sm font-medium text-blue-600 hover:underline">
+                        Esqueceu a Senha?
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    );
+}
