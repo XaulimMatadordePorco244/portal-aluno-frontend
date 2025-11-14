83a3423425ca38ad6b14371cd7f1ed53b7dfded3
XaulimMatadordePorco12 <michael.silva3@estudante.ifms.edu.br>
2025-11-14 16:03:12 -0400

    Feat: cria uma rota de Soft Delete

---
+++ src/app/api/user/delete/route.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+const prisma = new PrismaClient();
+
+
+export async function handleDeleteUser(userId: string) {
+  
+  if (!userId) {
+    throw new Error("ID do usuário é obrigatório");
+  }
+
+  try {
+    const usuarioInativado = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+              status: 'INATIVO', 
+            passwordResetToken: null,
+        passwordResetExpires: null,
+      },
+    });
+
+    console.log("Usuário inativado com sucesso:", usuarioInativado.id);
+    return usuarioInativado;
+
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+      console.error("Usuário não encontrado para inativar.");
+    } else {
+      console.error("Erro ao inativar usuário:", error);
+    }
+    throw error;
+  }
+}
