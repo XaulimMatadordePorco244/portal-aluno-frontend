6d0957a19c584234c8c8c4e36e9aadaafd212c78
XaulimMatadordePorco12 <michael.silva3@estudante.ifms.edu.br>
2025-11-25 16:15:16 -0400

    fix: corrige cores do Input para respeitar tema dark/light

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils" 
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>; { }
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -9,17 +9,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-                    "flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background",
           "file:border-0 file:bg-transparent file:text-sm file:font-medium",
-          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring ",
+          "placeholder:text-muted-foreground",
+          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
           "disabled:cursor-not-allowed disabled:opacity-50",
-          "bg-black/5",
-          "placeholder:text-gray-500",
-          "not-placeholder-shown:text-black",
-          "not-placeholder-shown:border-black",
-          "focus:border-black",
-          "transition-colors",
-
+          "text-foreground",
           className
         )}
         ref={ref}
