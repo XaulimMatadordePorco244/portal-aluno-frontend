7f48763ad30bb3c83235862ff9b6344d50dd8522
XaulimMatadordePorco12 <michael.silva3@estudante.ifms.edu.br>
2026-03-06 13:55:28 -0400

    fix: correção de link de navegação

--- src/app/admin/materiais/[id]/page.tsx
+++ src/app/admin/materiais/[id]/page.tsx
@@ -64,7 +64,7 @@ export default async function DetalhesMaterialAdminPage({
   };
 
   return (
-    <div className="p-6 max-w-7xl mx-auto space-y-8">
+    <div className=" space-y-8">
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
         <div>
           <Link href="/admin/materiais" className="text-muted-foreground hover:text-foreground transition-colors inline-flex items-center text-sm font-medium mb-2">
